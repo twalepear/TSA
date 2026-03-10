db4a077499b934cd75b9fb663ffbf3c898302119
twalepear <twale.pear@gmail.com>
2026-03-10 14:11:27 +1030

    attempt to create tsibble object

--- Assignment 1 Part 1.R
+++ Assignment 1 Part 1.R
@@ -1,4 +1,17 @@
 library(fpp3)
+library(tsibble)
+library(tidyverse)
+library(lubridate)
 
 BirthAndFertility <- readr::read_csv("BirthsAndFertilityRatesAnnual.csv")
 
+BirthAndFertility_t <- BirthAndFertility |> # the data is transposed
+  t() |>
+  as.data.frame() |>
+  tibble::rownames_to_column("DataSeries")
+  select(`Total Fertililty Rate (TFR)`,
+         `Total Live-Births`) |>
+  mutate(Year = year(DataSeries))
+  as_tsibble(
+    index = Year
+  )
