229d4f1773908740c923abb409aebd840f932f04
Thoa Thanh Thi Le <a1847933@adelaide.edu.au>
2026-03-10 18:31:43 +1030

    BirthAndFertility data by itself

--- Assignment 1 Part 1.R
+++ Assignment 1 Part 1.R
@@ -3,15 +3,19 @@ library(tsibble)
 library(tidyverse)
 library(lubridate)
 
-BirthAndFertility <- readr::read_csv("BirthsAndFertilityRatesAnnual.csv")
+data <- readr::read_csv("BirthsAndFertilityRatesAnnual.csv")
 
-BirthAndFertility_t <- BirthAndFertility |> # the data is transposed
-  t() |>
-  as.data.frame() |>
-  tibble::rownames_to_column("DataSeries")
-  select(`Total Fertililty Rate (TFR)`,
-         `Total Live-Births`) |>
-  mutate(Year = year(DataSeries))
-  as_tsibble(
-    index = Year
+BirthAndFertility <- data |>
+  pivot_longer(
+    cols = `1960`:`2025`,
+    names_to = "Year",
+    values_to = "Value",
+    values_transform = list(Value = as.numeric)
+  ) |>
+  filter(DataSeries %in% c("Total Fertility Rate (TFR)", "Total Live-Births")) |>
+  mutate(Year = as.integer(Year)) |>
+  filter(Year != 2025) |>
+  pivot_wider(
+    names_from = DataSeries,
+    values_from = Value
   )
