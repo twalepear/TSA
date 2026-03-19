0477d11cea6dc3107cb06b0a46dbb9f2c923a7e2
twalepear <145573584+twalepear@users.noreply.github.com>
2026-03-19 15:14:21 +1030

    trying to make a tsibble object

--- Assignment 1 Part 1.R
+++ Assignment 1 Part 1.R
@@ -6,16 +6,29 @@ library(lubridate)
 data <- readr::read_csv("BirthsAndFertilityRatesAnnual.csv")
 
 BirthAndFertility <- data |>
-  pivot_longer(
+  pivot_longer( # change rows to column
     cols = `1960`:`2025`,
     names_to = "Year",
-    values_to = "Value",
+    values_to = "Value", # naming it to remember that it needs to be values
     values_transform = list(Value = as.numeric)
   ) |>
   filter(DataSeries %in% c("Total Fertility Rate (TFR)", "Total Live-Births")) |>
   mutate(Year = as.integer(Year)) |>
-  filter(Year != 2025) |>
-  pivot_wider(
-    names_from = DataSeries,
+  filter(Year != 2025) |> # assignment specified until 2024
+  pivot_wider( 
+    names_from = DataSeries, 
     values_from = Value
   )
+
+BirthAndFertility <- BirthAndFertility |>
+  mutate(Year = year(Year)) |>
+  as_tsibble(key = c(`Total Fertility Rate (TFR)`, `Total Live-Births`),
+             index = Year)
+
+# Preliminary (exploratory analysis) {then choosing and fitting models, then using & evaluating a forecasting model}
+# patterns?
+# significant trend?
+# seasonality important?
+# evidence of the presence of business cycles?
+# outliers that need to be explained by experts?
+# strength of relationships among variables?
